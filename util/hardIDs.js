278ff6a5926dc08b7cb5cf82acc67320768a908d
Aubin C. Spitzer <aubin@uthd.dev>
2021-06-04 16:52:24 -0700

    from laptop

---
+++ util/hardIDs.js
@@ -0,0 +1,4 @@
+exports.compCaptainRoleIds = [process.env.ROLE_ID_COMP_ONE, process.env.ROLE_ID_COMP_TWO];
+exports.compVcIds = [process.env.VC_ID_COMP_ONE, process.env.VC_ID_COMP_TWO];
+exports.scrimCaptainRoleIds = [process.env.ROLE_ID_SCRIM_ONE, process.env.ROLE_ID_SCRIM_TWO];
+exports.scrimVcIds = [process.env.VC_ID_SCRIM_ONE, process.env.ROLE_ID_SCRIM_TWO];
